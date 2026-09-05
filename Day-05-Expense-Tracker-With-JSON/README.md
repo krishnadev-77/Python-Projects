03d25d5412a13af1b010c7883c634690cf2f3254
krishna66-dev <techiegig76@gmail.com>
2026-09-05 16:19:32 +0530

    Day-05-Expense-Tracker-With-JSON

---
+++ Day-05-Expense-Tracker-With-JSON/README.md
@@ -0,0 +1,39 @@
+# 💰 Python Expense Tracker with JSON
+
+A command-line Expense Tracker built with Python that allows users to add, view, calculate, delete, save, and load expenses.
+
+This version improves the basic Expense Tracker by using **JSON file storage**, allowing expenses to be saved and loaded even after the program is closed.
+
+## 🚀 Features
+
+- Add expenses
+- View all expenses
+- Calculate total expenses
+- Delete expenses
+- Save expenses to a JSON file
+- Load expenses from a JSON file
+- Basic invalid input handling
+- Menu-driven command-line interface
+
+## 🛠️ Technologies Used
+
+- Python 
+- JSON
+- Lists
+- Dictionaries
+- Loops
+- Conditional statements
+- Functions
+- File Handling
+- `json.dump()`
+- `json.load()`
+- `enumerate()`
+
+## 📂 Project Structure
+
+```text
+Day-05-Expense-Tracker-With-JSON/
+│
+├── main.py
+├── expenses.json
+└── README.md
--- Day-05-Expense-Tracker-With-JSON/main.py
+++ Day-05-Expense-Tracker-With-JSON/main.py
@@ -2,6 +2,7 @@
 Project:- 04
 Expense Tracker
 '''
+import json
 
 print("====================")
 print(" Expense Tracker")
@@ -13,7 +14,10 @@ while True:
     print("1. Add Expense")
     print("2. View Expense")
     print("3. Total Expense")
-    print("4. Exit")
+    print("4. Delete Expense")
+    print("5. Save Expense")
+    print("6. Load Expense")
+    print("7. Exit")
 
     choice = input("Enter your choice :")
 
@@ -38,6 +42,19 @@ while True:
             total = total + expense["Amount"]
         print("Total Amount" , total)
     elif choice == "4":
+        removal = int(input("Enter the expense you want to Delete :"))
+        expenses.pop(removal-1)
+    elif choice == "5":
+        with open("expenses.json" , "w")as file:
+            json.dump(expenses,file)
+    elif choice == "6":
+        try:
+            with open("expenses.json"  , "r")as file:
+                expenses = json.load(file)
+            print("File Loaded Successfully!!")
+        except FileNotFoundError:
+            print("No saved expenses found!!")
+    elif choice == "7":
         print("Thanks for using Expense Tracker!!")
         break
     else:
